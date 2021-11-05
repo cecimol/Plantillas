68e23f1b9d1b69b9aefc04f224e22fe186fd6eeb
CardCaptor <cardcaptor@pop-os.localdomain>
2021-11-04 21:34:52 -0300

    agregue pug

--- handlebars/server.js
+++ handlebars/server.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.set("view engine", "hbs");
-app.set("views", "./views/handlebars");
+app.set("views", "./views");
 
 app.engine(
   "hbs",
